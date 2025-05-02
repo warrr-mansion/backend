45f30c01db928d94fcd4e386628fdd861e542f27
Gyuchan Kim <sb3g@naver.com>
2025-05-02 15:24:38 +0900

    🎉 init: Create initial project structure

---
+++ src/main/java/com/warrr/zipflex/ZipflexApplication.java
@@ -0,0 +1,13 @@
+package com.warrr.zipflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipflexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/warrr/zipflex/ZipflexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warrr.zipflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
