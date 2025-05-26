5b83fcab9ba5f1dcd23c544167c5c718fc8b7156
Gyuchan Kim <sb3g@naver.com>
2025-05-26 17:13:01 +0900

    🐛 fix: Temporarily add setter to PageRequestDto

--- src/main/java/com/warrr/zipflex/global/support/PageRequestDto.java
+++ src/main/java/com/warrr/zipflex/global/support/PageRequestDto.java
@@ -2,26 +2,21 @@ package com.warrr.zipflex.global.support;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
-import lombok.experimental.SuperBuilder;
 
 @Getter
+@Setter
 @ToString
-@SuperBuilder
-@AllArgsConstructor
 @NoArgsConstructor
 public class PageRequestDto {
     
     @Schema(defaultValue = "1", description = "조회할 페이지 번호 (1부터 시작)")
-    @Builder.Default
     private int pageNo = 1;
 
     @Schema(defaultValue = "10", description = "페이지당 항목 수")
-    @Builder.Default
     private int pageSize = 10;
 
     @JsonIgnore
